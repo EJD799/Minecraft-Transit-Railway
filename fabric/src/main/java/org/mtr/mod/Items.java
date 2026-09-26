95ff43f1ffafb007f1769ef49c8bff04792b9393
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-09-26 16:15:48 +0900

    Add RAIL_CONNECTOR_HELICOPTER item registration

--- fabric/src/main/java/org/mtr/mod/Items.java
+++ fabric/src/main/java/org/mtr/mod/Items.java
@@ -70,6 +70,7 @@ public final class Items {
 		RAIL_CONNECTOR_TURN_BACK = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_turn_back"), itemSettings -> new Item(new ItemRailModifier(true, false, true, false, RailType.TURN_BACK, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_CABLE_CAR = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_cable_car"), itemSettings -> new Item(new ItemRailModifier(false, true, false, true, RailType.CABLE_CAR, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_RUNWAY = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_runway"), itemSettings -> new Item(new ItemRailModifier(false, false, true, true, RailType.RUNWAY, itemSettings)), CreativeModeTabs.CORE);
+		RAIL_CONNECTOR_HELICOPTER = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_helicopter"), itemSettings -> new Item(new ItemRailModifier(true, true, true, false, RailType.HELICOPTER, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_REMOVER = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_remover"), itemSettings -> new Item(new ItemRailModifier(itemSettings)), CreativeModeTabs.CORE);
 
 		// Signal connectors
